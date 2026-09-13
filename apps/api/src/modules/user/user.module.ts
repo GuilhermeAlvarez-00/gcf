b4a0d61f90c7ea7857a8d198db7016ecfb441bc0
Guilherme Alvarez <gui06barboza@gmail.com>
2026-09-12 21:04:03 -0300

    feat(module|user): creating user module

---
+++ apps/api/src/modules/user/user.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { CreateUserUseCase } from "./use-cases/create-user/create-user.usecase";
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class UserModule {}
